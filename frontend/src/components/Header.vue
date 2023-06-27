d1513753d2f7a645327151490dde835702018a8c
Thomas Pamantung <pamantungthomas@gmail.com>
2023-06-27 16:34:44 -0400

    Premier commit, files: Footer, Header, Home, ImageGallery, Courses

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+const image = ref('../assets/umich-logo.jpg')
+</script>
+
+<template>
+    <v-app-bar app color="#00274c" dark>
+        <v-img :src="image"></v-img>
+        <v-btn color="white">Esiea Web Tech</v-btn>
+        <v-btn color="white">Login</v-btn>
+        <v-btn color="white">Register</v-btn>
+    </v-app-bar>
+</template>
