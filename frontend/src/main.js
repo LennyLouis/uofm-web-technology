a82b1c0ceacf782955894f7453a4afe61ad583b6
menardmalo <malo.menard02@gmail.com>
2023-06-28 17:01:11 -0400

    add front

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,27 +6,30 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// MDI
+// Vuetify
 import '@mdi/font/css/materialdesignicons.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-
-// Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import * as labsComponents from 'vuetify/labs/components'
 
-// Vietify
 const vuetify = createVuetify({
-    components,
-    directives,
+  theme:{
+    defaultTheme: 'light'
+  },
+  components: {
+    ...components,
+    ...labsComponents,
+  },
+  directives,
 })
 
 const app = createApp(App)
 
-
 app.use(createPinia())
-app.use(router)
 app.use(vuetify)
+app.use(router)
 
 app.mount('#app')
