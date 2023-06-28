f7cac57f78c7a4073b3847b73f66dafb7f8ef787
Thomas Pamantung <pamantungthomas@gmail.com>
2023-06-28 13:59:03 -0400

    Troisième commit, ajout du gray scale switch et input field. Création des fichiers : Carousel, AddPhotoForm, Panel.

---
+++ frontend/src/components/AddPhotoForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-sheet width="300" class="mx-auto">
+        <v-form fast-fail @submit.prevent>
+            <v-text-field v-model="imageName" label="Enter image name" :rules="imageNameRules"></v-text-field>
+
+            <v-text-field v-model="imageDesc" label="Enter image description" :rules="imageDescRules"></v-text-field>
+            
+            <v-text-field v-model="imageUrl" label="Enter image URL" :rules="imageUrlRules"></v-text-field>
+
+            <v-btn type="submit" block class="mt-2">ADD</v-btn>
+        </v-form>
+    </v-sheet>
+</template>
+<script>
+export default {
+    data: () => ({
+        imageName: '',
+        imageNameRules: [
+            value => {
+                if (/^[a-zA-Z0-9]+$/.test(value)) return true
+
+                return 'Image name must contain only letters and numbers and not be empty.'
+            },
+        ],
+        imageDesc: '',
+        imageDescRules: [
+            value => {
+                if (/^[a-zA-Z0-9]+$/.test(value)) return true
+
+                return 'Image description must contain only letters and numbers and not be empty.'
+            },
+        ],
+        imageUrl: '',
+        imageUrlRules: [
+            value => {
+                // rule for a valid url 
+                if (/^(ftp|http|https):\/\/[^ "]+$/.test(value)) return true
+
+                return 'Image URL must be a valid URL and not be empty.'
+            },
+        ],
+    }),
+}
+</script>
--- frontend/src/components/ImageGallery.vue
+++ frontend/src/components/ImageGallery.vue
@@ -1,22 +1,41 @@
-
+<script>
+export default {
+    data: () => ({
+        nbOfImages: '10',
+        grayScaleBool: '',
+    }),
+}
+</script>
 
 <template>
+    <v-row>
+        <v-responsive class="mx-auto" max-width="344">
+            <v-text-field v-model="nbOfImages" clearable hide-details="auto" label="Number of images"></v-text-field>
+        </v-responsive>
+        <v-switch v-model="grayScaleBool" class="ml-6" label="Gray Scale"></v-switch>
+    </v-row>
+
     <v-card class="mx-5 my-2 pa-3">
         <v-row>
-            <v-col v-for="n in 18" :key="n" class="d-flex child-flex" cols="3" >
-              
-                <v-card >
-                <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                    :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`" aspect-ratio="1" cover
-                    class="bg-grey-lighten-2">
-                    <template v-slot:placeholder>
-                        <v-row class="fill-height ma-0" align="center" justify="center">
-                            <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
-                        </v-row>
-                    </template>
-                </v-img>
-            </v-card>
+            <v-col v-for="n in parseInt(nbOfImages)" :key="n" class="d-flex child-flex" cols="3">
+                <v-card>
+                    <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`" aspect-ratio="1" cover
+                        :class="{ grayscale: grayScaleBool }" class="bg-grey-lighten-2">
+                        <template v-slot:placeholder>
+                            <v-row class="fill-height ma-0" align="center" justify="center">
+                                <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
+                            </v-row>
+                        </template>
+                    </v-img>
+                </v-card>
             </v-col>
         </v-row>
     </v-card>
 </template>
+
+<style scoped>
+.grayscale {
+    filter: grayscale(100%);
+}
+</style>
